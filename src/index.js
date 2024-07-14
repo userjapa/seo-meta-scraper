d6ed64e0d554bfcedd84357af84e038f4c661111
willian.hsimabukuro <willian.hsimabukuro@gmail.com>
2024-07-14 02:55:07 -0300

    fixed env var

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const express    = require('express'),
 const https = require('node:https')
 const { JSDOM } = require('jsdom')
 
-const PORT = 8080
+const PORT = process.env.PORT || 8080
 
 const app = express()
 
